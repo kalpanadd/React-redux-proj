40f2027c86aa887116c10f156114b07fa08303e7
kalpana1 <dkdevi.dkd@gmail.com>
2021-05-28 22:24:01 +0530

    component

--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const ProductComponent = () => {
+
+    const products = useSelector((state) => state.allProducts.products)
+    console.log(products);
+
+    const { id, title } = products[0];
+
     return (
-        <div>
-            <h1>ProductComponent</h1>
+        <div className='four column wide'>
+            <div className='ui link card'>
+                <div className='card'>
+                    <div className='image'>
+                        <div className='content'>
+                            <div className='header'>{title}</div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
