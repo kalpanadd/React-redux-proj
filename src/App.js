8165efaf8d026f798f54850ccf3664bd4864a1b3
kalpana1 <dkdevi.dkd@gmail.com>
2021-05-28 20:36:24 +0530

    accessed store in listing component using useSelector

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
-
+import Header from './containers/Header';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ProductListing from './containers/ProductListing';
+import ProductDetails from './containers/ProductDetails';
 function App() {
   return (
     <div className="App">
-      <h1> hello world</h1>
+      <Header />
 
+      <Router>
+        <Switch>
+
+          <Route path='/' exact component={ProductListing} />
+          <Route path='/product/:productId' component={ProductDetails} />
+          <Route>404 NOT FOUND!</Route>
+
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/containers/ProductDetails.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProductDetails() {
+    return (
+        <div className='ui grid container'>
+            <h1>ProductDetails</h1>
+        </div>
+    )
+}
+
+export default ProductDetails
